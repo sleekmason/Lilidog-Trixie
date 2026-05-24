e997f37dcc53e5e432963168602db39a320a29a5
sleekmason <sleekmason@gmail.com>
2026-05-24 09:33:13 -0500

    Welcome text misc.

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -9,13 +9,13 @@ if grep -q "$fs" /proc/partitions; then
 --width=428 --height=414 --center --escape-ok --undecorated --skip-taskbar \
 --button=" Begin!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:x-terminal-emulator -T 'Customization' -e 'sudo ld-entry -i'" \
 --button=" Exit!application-exit:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Light 10" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="JetBrains Mono Regular 10" \
 --fore="#DAE4E8"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; if ! grep hypervisor /proc/cpuinfo ; then sed -i '/vmrestore.*/d' ~/.config/openbox/autostart; fi; exit
 else
 /usr/local/bin/live-session &&
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
---width=428 --height=392 --center --escape-ok --undecorated --skip-taskbar \
+--width=428 --height=392 --center --escape-ok  --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
---text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Light 10" \
+--text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="JetBrains Mono Regular 10" \
 --fore="#DAE4E8"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart; exit
 fi
